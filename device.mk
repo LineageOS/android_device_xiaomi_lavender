635c344216130772277b5c01b4f3385c965f5e1d
Davide Garberi <dade.garberi@gmail.com>
2020-03-14 13:36:51 +0100

    lavender: Decommonize USB hal

--- device.mk
+++ device.mk
@@ -72,5 +72,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libcamera_sdm660_shim
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Inherit proprietary files
 $(call inherit-product-if-exists, vendor/xiaomi/lavender/lavender-vendor.mk)
