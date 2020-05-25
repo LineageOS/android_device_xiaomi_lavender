837dc060ee69f2a12d5f1e1e535eaa8bd8657393
Usaamah Patel <ussyp2015@gmail.com>
2020-05-25 19:33:43 +0100

    lavender: releasetools: Don't include/flash DTBO image as this breaks boot
    Change-Id: I76f6261ab3b86b30112bb51710ad70863a123d4b

--- releasetools.py
+++ releasetools.py
@@ -32,5 +32,4 @@ def AddImage(info, basename, dest):
 
 def OTA_InstallEnd(info):
   info.script.Print("Patching device-tree and verity images...")
-  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
