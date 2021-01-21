e4a7b0c7df0ade6d42b274f12f3bb0ad9e4a28c7
SakilMondal <mondalsakil@gmail.com>
2021-01-21 12:56:16 +0530

    lavender: Drop non-functional soundtrigger configs
    * Fixes google assistant voice match in android 11
    
    Change-Id: I8d20c39f99ba871c327ab74fede4de721f1a4f33

--- device.mk
+++ device.mk
@@ -12,9 +12,7 @@ DEVICE_PATH := device/xiaomi/lavender
 # Audio
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
-    $(DEVICE_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
-    $(DEVICE_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
-    $(DEVICE_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
+    $(DEVICE_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # Camera
 PRODUCT_PACKAGES += \
