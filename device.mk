444966fe621f3c50f5f6a55cba1552a4d5ccb90b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-20 11:31:14 +0100

    lavender: Decommonize IR
    Change-Id: If5d83ef3dc0169700fcd738f6a50cb38b104c30a

--- device.mk
+++ device.mk
@@ -40,6 +40,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libstdc++.vendor
 
+# Consumerir
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+
 # FM
 BOARD_HAVE_QCOM_FM := true
 
