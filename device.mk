d958322d17a8c285ba11efae0416adf86d9e04d3
SebaUbuntu <barezzisebastiano@gmail.com>
2021-12-20 12:26:32 +0100

    lavender: Move to common IR impl
    Change-Id: Ib1dbba68aa3227fe4fa0e539717adedad2f08d21

--- device.mk
+++ device.mk
@@ -21,12 +21,7 @@ PRODUCT_PACKAGES += \
     libstdc++.vendor
 
 # Consumerir
-PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+BOARD_HAVE_IR := true
 
 # FM
 BOARD_HAVE_QCOM_FM := true
