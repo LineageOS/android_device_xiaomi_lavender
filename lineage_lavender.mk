b2e5adf0e8b6d465adcd6e71ecf426946c4d2389
Giovix92 <ggualtierone@gmail.com>
2022-01-10 00:52:04 +0100

    lavender: Update build fingerprint
    Change-Id: Ia8f9b4bf997a1f7254e9d8da66cfebc2962a594f

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -26,6 +26,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := lavender
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 V12.0.1.0.QFGMIXM release-keys"
+    PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 V12.5.3.0.QFGCNXM release-keys"
 
-BUILD_FINGERPRINT := xiaomi/lavender/lavender:10/QKQ1.190910.002/V12.0.1.0.QFGMIXM:user/release-keys
+BUILD_FINGERPRINT := xiaomi/lavender/lavender:10/QKQ1.190910.002/V12.5.3.0.QFGCNXM:user/release-keys
